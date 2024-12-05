f9aa23d3baed13538bf8ee1b11f11b9f3b08c818
lindx <lindx@qq.com>
2024-12-05 20:23:17 +0800

    33

--- src/test/java/Hellow.java
+++ src/test/java/Hellow.java
@@ -2,5 +2,6 @@ public class Hellow {
     public static void main(String[] args){
         System.out.println("hello word");
         System.out.println("2 2");
+        System.out.println("33");
     }
 }
